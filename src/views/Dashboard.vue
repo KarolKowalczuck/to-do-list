c4aa7e39deb949b40ca1356ababf9365792bc206
Karolina <karolinaborgesk@gmail.com>
2023-06-28 15:56:17 -0300

    estilização das paginas das listas

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -20,10 +20,9 @@ export default {
         const { data } = await this.list();
         this.toDoLists = data;
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
     },
-
   },
   mounted() {
     this.getLists();
@@ -31,14 +30,86 @@ export default {
 };
 </script>
 <template>
-  <v-card v-for="list in toDoLists" :key="list.id">
-    <router-link :to="`/list-detail/${list.id}`">
-      <v-card-title> {{ list.title }}</v-card-title>
-    </router-link>
-  </v-card>
-<br/>
-  <v-btn>
-    <router-link to="/create-list">criar</router-link>
-  </v-btn>
-
+  <v-sheet
+    class="d-flex align-center justify-center overflow-hidden bg-brown-lighten-5"
+    rounded
+    height="100vh"
+  >
+    <v-card
+      class="d-md-xxl-mx-auto px-6 py-6 bg-grey-lighten-5"
+      height="80vh"
+      width="70vw"
+      rounded="xl"
+      elevation="16"
+    >
+      <p class="text-h5 font-weight-medium text-brown-darken-1 ml-6 mb-4">
+        Your Lists:
+      </p>
+      <v-divider/>
+      <v-card 
+      class="bg-transparent overflow-auto mt-4" 
+      elevation="0"
+      height="78%">
+        <div
+          v-for="list in toDoLists"
+          :key="list.id"
+          class="titulos mx-auto ma-5"
+          width="90%"
+        >
+          <v-hover v-slot="{ isHovering, props }" open-delay="200">
+            <v-card
+              :elevation="isHovering ? 8 : 4"
+              :class="{ 'on-hover': isHovering }"
+              class="mx-auto bg-brown-lighten-5"
+              height="100%"
+              max-width="95%"
+              rounded="lg"
+              v-bind="props"
+            >
+              <router-link
+                class="text-decoration-none text-pink-lighten-3"
+                :to="`/list-detail/${list.id}`"
+              >
+                <v-card-title> {{ list.title }}</v-card-title>
+              </router-link>
+            </v-card>
+          </v-hover>
+        </div>
+      </v-card>
+      <br/>
+      <v-divider />
+      <br/>
+      <div
+      class="d-flex justify-end">
+        <v-btn
+        class="rounded-xl"
+        color="teal-lighten-3"
+        size="large"
+        variant="elevated"
+        elevation="4"
+        height="50px"
+        width="200px"
+        >
+        <router-link 
+        class="text-decoration-none text-white font-weight-bold"
+        to="/create-list">criar</router-link>
+      </v-btn>
+    </div>
+    </v-card>
+  </v-sheet>
 </template>
+
+<style scoped>
+.titulos {
+  height: 55px;
+}
+::-webkit-scrollbar {
+  width: 10px;
+  box-shadow: inset 0 0 2px #F8BBD0;
+  border-radius: 5px;
+}
+::-webkit-scrollbar-thumb {
+  background: #F8BBD0;
+  border-radius: 5px;
+}
+</style>
--- src/views/DetailList.vue
+++ src/views/DetailList.vue
@@ -32,15 +32,14 @@ export default {
         console.log(err);
       }
     },
-    async updatedTask(item){
+    async updatedTask(item) {
       try {
         let updatetask = {
-          done: item.done
-        }
-      await this.updateItem(item.id, updatetask)
-      } 
-        catch (err) {
-        console.log(err)
+          done: item.done,
+        };
+        await this.updateItem(item.id, updatetask);
+      } catch (err) {
+        console.log(err);
       }
     },
   },
@@ -51,70 +50,82 @@ export default {
 </script>
 
 <template>
-  <v-container>
-    <h1>{{ listTitle }}</h1>
-  </v-container>
+  <v-sheet
+    class="d-flex flex-column align-center justify-center overflow-auto bg-brown-lighten-5"
+    rounded
+    height="100vh"
+  >
+    <v-card
+      class="bg-grey-lighten-5 text-brown-darken-1 ma-4"
+      rounded="xl"
+      elevation="16"
+      width="80vw"
+      height="80vh"
+    >
+      <v-container>
+        <h1 class="font-weight-medium text-brown-darken-1 ml-4 mb-4">
+          {{ listTitle }}</h1>
+      </v-container>
 
-  <v-container>
-  
-    <v-card v-if="items.length > 0">
-      <template v-for="(item, i) in items" :key="`${i}-${item.text}`">
-        <v-divider v-if="i !== 0" :key="`${i}-divider`"></v-divider>
-        <v-list-item>
-          <template v-slot:prepend>
-            <v-checkbox-btn
-                @change="updatedTask(item)"
-              v-model="item.done"
-              color="success"
-            ></v-checkbox-btn>
+      <v-container>
+        <v-card v-if="items.length > 0">
+          <template v-for="(item, i) in items" :key="`${i}-${item.text}`">
+            <v-divider v-if="i !== 0" :key="`${i}-divider`"></v-divider>
+            <v-list-item>
+              <template v-slot:prepend>
+                <v-checkbox-btn
+                  @change="updatedTask(item)"
+                  v-model="item.done"
+                  color="teal-lighten-2"
+                ></v-checkbox-btn>
+              </template>
+              <v-list-item-title>
+                <span :class="item.done ? 'text-grey' : 'text-primary'">
+                  {{ item.title }}
+                </span>
+              </v-list-item-title>
+              <template v-slot:append>
+                <v-icon v-if="item.done" color="teal-lighten-2">mdi-check</v-icon>
+              </template>
+            </v-list-item>
           </template>
-          <v-list-item-title>
-            <span :class="item.done ? 'text-grey' : 'text-primary'">
-              {{ item.title }}
-            </span>
-          </v-list-item-title>
-          <template v-slot:append>
-            <v-icon v-if="item.done" color="success">mdi-check</v-icon>
-          </template>
-        </v-list-item>
-      </template>
-    </v-card>
-    <br />
-
-    <v-btn>
-      <router-link :to="`/edit-list/${listId}`">Editar Lista </router-link>
-    </v-btn>
-      <v-btn @click="setupModal(deleteList)"> Deletar Lista 
-      <v-dialog v-model="dialog" activator="parent" width="auto">
-        <v-card>
-          <v-card-text>
-            <h3>Deletar Lista</h3>
-      <p>Tem certeza que quer deletar a Lista '{{ listTitle}}'?</p>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn  @click="dialog = false">Cancelar</v-btn>
-            <v-btn  @click="deleteList">Deletar</v-btn>
-          </v-card-actions>
         </v-card>
-       </v-dialog>
-       </v-btn>
-    <v-btn>
-      <router-link to="/dashboard">Home </router-link>
-    </v-btn>
-  </v-container>
+        <br />
 
-  <Modal v-show="showModal"> 
-    <div class="modal-content">
-      <h3>Deletar Lista</h3>
-      <p>Tem certeza que quer deletar a Lista '{{ listTitle}}'?</p>
+        <v-btn>
+          <router-link :to="`/edit-list/${listId}`">Edit</router-link>
+        </v-btn>
+        <v-btn @click="setupModal(deleteList)">
+          Delete
+          <v-dialog v-model="dialog" activator="parent" width="auto">
+            <v-card>
+              <v-card-text>
+                <h3>Delete List</h3>
+                <p>Tem certeza que quer deletar a Lista '{{ listTitle }}'?</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn @click="dialog = false">No</v-btn>
+                <v-btn @click="deleteList">Yes</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-btn>
+        <v-btn>
+          <router-link to="/dashboard">Home </router-link>
+        </v-btn>
+      </v-container>
 
-      <div class="modal-actions">
-        <button class="btn-cancelar" @click="setupModal">cancelar</button>
-        <button class="btn-confirmar" @click="deleteList">confirmar</button>
-      </div>
-    </div>
-  
-  </Modal>
-    
+      <Modal v-show="showModal">
+        <div class="modal-content">
+          <h3>Deletar Lista</h3>
+          <p>Tem certeza que quer deletar a Lista '{{ listTitle }}'?</p>
 
+          <div class="modal-actions">
+            <button class="btn-cancelar" @click="setupModal">cancelar</button>
+            <button class="btn-confirmar" @click="deleteList">confirmar</button>
+          </div>
+        </div>
+      </Modal>
+    </v-card>
+  </v-sheet>
 </template>
