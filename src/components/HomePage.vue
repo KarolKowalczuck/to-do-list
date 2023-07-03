c9c16dafd4c53414f0c557e2748978f2d30fd23f
Karolina <karolinaborgesk@gmail.com>
2023-07-03 11:29:50 -0300

    adiciona imagem de fundo

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,11 +4,11 @@
   <div
     class="d-flex align-center justify-center h-100 overflow-hidden bg-grey-lighten-5"
   >
-    <v-responsive class="align-center justify-center text-center fill-height">
-      <h1 class="text-brown-lighten-2">To do list</h1>
-      <br />
+    <v-container class="container d-flex flex-column align-center justify-center text-center fill-height">
+      <img src="../components/images/coffecroqui.png" class="teste" />
+      <h1 class="text-brown-lighten-1">To do list</h1>
 
-      <v-row class="d-flex align-center justify-center">
+      <v-row class="links d-flex align-center justify-center">
         <v-col cols="auto">
           <router-link
             to="/register"
@@ -26,7 +26,7 @@
           >
         </v-col>
       </v-row>
-    </v-responsive>
+    </v-container>
   </div>
 </template>
 
@@ -35,18 +35,39 @@ h1 {
   font-family: "Architects Daughter", cursive;
   font-size: 140px;
   font-weight: lighter;
+  z-index: 2;
+  margin-top: 300px;
+  padding-bottom: 0px;
 }
 
 .link {
   font-size: 1.7rem;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+.container{
+  overflow: hidden;
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+}
+.teste {
+  width: 100%;
+  height: auto;
+  margin-bottom: 50px;
+  opacity: 8%;
+  z-index: 1;
+
+  position: absolute;
 }
 
 @media (max-width: 500px) {
-  h1{
+  h1 {
     font-size: 70px;
+    margin-top: 120px;
   }
-  .link{
-    font-size: 1rem
+  .link {
+    font-size: 1rem;
   }
 }
 </style>
